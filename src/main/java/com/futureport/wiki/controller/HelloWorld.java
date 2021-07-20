ae8bc3ae87408ba3ac18b2f63ce28d33d4129470
jrt-20 <76930558+jrt-20@users.noreply.github.com>
2021-07-20 21:43:34 +0800

    2-5 第一个接口HelloWorld

---
+++ src/main/java/com/futureport/wiki/controller/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.futureport.wiki.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorld {
+
+    @RequestMapping("/hello")
+    public String hello(){
+        return "hello,World";
+    }
+}
