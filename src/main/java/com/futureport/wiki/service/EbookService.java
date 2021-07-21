ceac845be47ece5050e9775fd3d0dad683458ebe
jrt-20 <76930558+jrt-20@users.noreply.github.com>
2021-07-21 13:43:21 +0800

    3-7 CopyUtil封装转化功能

--- src/main/java/com/futureport/wiki/service/EbookService.java
+++ src/main/java/com/futureport/wiki/service/EbookService.java
@@ -5,6 +5,7 @@ import com.futureport.wiki.entity.EbookExample;
 import com.futureport.wiki.mapper.EbookMapper;
 import com.futureport.wiki.req.EbookReq;
 import com.futureport.wiki.resp.EbookResp;
+import com.futureport.wiki.utils.CopyUtil;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
@@ -23,15 +24,21 @@ public class EbookService {
         criteria.andNameLike("%"+req.getName()+"%");
         List<Ebook> list = ebookMapper.selectByExample(example);
 
-        List<EbookResp> lists = new LinkedList<>();
-        /**
-         * 遍历Ebook集合，转成EbookResp
-         */
-        for (Ebook ebook : list) {
-            EbookResp ebookResp = new EbookResp();
-            BeanUtils.copyProperties(ebook,ebookResp);
-            lists.add(ebookResp);
-        }
+//        List<EbookResp> lists = new LinkedList<>();
+//        /**
+//         * 遍历Ebook集合，转成EbookResp
+//         */
+//        for (Ebook ebook : list) {
+////            EbookResp ebookResp = new EbookResp();
+////            BeanUtils.copyProperties(ebook,ebookResp);
+              //对象复制
+//            EbookResp ebookResp = CopyUtil.copy(ebook,EbookResp.class);
+//            lists.add(ebookResp);
+//        }
+//        return lists;
+
+        //列表复制
+        List<EbookResp> lists = CopyUtil.copyList(list, EbookResp.class);
         return lists;
     }
 }
