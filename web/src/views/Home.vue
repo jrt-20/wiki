ee460f2f806de832291488efb78a4e742a304661
jrt-20 <76930558+jrt-20@users.noreply.github.com>
2021-07-22 16:26:38 +0800

    5-1 axios集成

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -56,8 +56,14 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import axios from 'axios'
 
 export default defineComponent({
   name: 'Home',
+  setup(){
+    axios.get("http://localhost/ebook/list?name=Python").then(function (response){
+      console.log(response);
+    });
+  },
 });
 </script>
