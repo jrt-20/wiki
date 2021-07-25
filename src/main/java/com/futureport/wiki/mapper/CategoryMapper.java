f68b9e6db58501c1d7201d38b77afeadb909268a
jrt-20 <76930558+jrt-20@users.noreply.github.com>
2021-07-25 22:32:57 +0800

    7-1 分类表设计

---
+++ src/main/java/com/futureport/wiki/mapper/CategoryMapper.java
@@ -0,0 +1,22 @@
+package com.futureport.wiki.mapper;
+
+import com.futureport.wiki.entity.Category;
+import com.futureport.wiki.entity.CategoryExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface CategoryMapper {
+    long countByExample(CategoryExample example);
+
+    int deleteByExample(CategoryExample example);
+
+    int insert(Category record);
+
+    int insertSelective(Category record);
+
+    List<Category> selectByExample(CategoryExample example);
+
+    int updateByExampleSelective(@Param("record") Category record, @Param("example") CategoryExample example);
+
+    int updateByExample(@Param("record") Category record, @Param("example") CategoryExample example);
+}
