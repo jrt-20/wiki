64af5f29a1cbb8a6a91831bace07e84397a4be89
jrt-20 <76930558+jrt-20@users.noreply.github.com>
2021-07-31 16:31:32 +0800

    8-12 添加预览功能

--- web/src/views/admin/admin-doc.vue
+++ web/src/views/admin/admin-doc.vue
@@ -96,12 +96,24 @@
             <a-form-item>
               <a-input v-model:value="doc.sort" placeholder="顺序"/>
             </a-form-item>
+
+            <a-form-item>
+              <a-button type="primary" @click="handlePreviewContent()">
+                <EyeOutlined/>
+                内容预览
+              </a-button>
+            </a-form-item>
+
             <a-form-item>
               <div id="content"></div>
             </a-form-item>
           </a-form>
         </a-col>
       </a-row>
+
+      <a-drawer :closable="false" :visible="drawerVisible" placement="right" width="900" @close="onDrawerClose">
+        <div :innerHTML="previewHtml" class="wangeditor"></div>
+      </a-drawer>
 
     </a-layout-content>
 
@@ -365,6 +377,19 @@ export default defineComponent({
       });
     }
 
+    /**
+     * 富文本预览
+     */
+    const drawerVisible = ref(false);
+    const previewHtml = ref();
+    const handlePreviewContent = () => {
+      const html = editor.txt.html();
+      previewHtml.value = html;
+      drawerVisible.value = true;
+    };
+    const onDrawerClose = () => {
+      drawerVisible.value = false;
+    };
 
 
 
@@ -396,6 +421,10 @@ export default defineComponent({
       treeSelectData,
       showConfirm,
       handleSave,
+      drawerVisible,
+      previewHtml,
+      handlePreviewContent,
+      onDrawerClose,
     }
   }
 });
