9eee63bc7cd2ee2e0c983526a19cb6eba847f466
jrt-20 <76930558+jrt-20@users.noreply.github.com>
2021-08-02 00:01:30 +0800

    10-6 增加定时器,定时执行电子书信息更新 SQL

--- src/main/java/com/futureport/wiki/mapper/DocMapperCust.java
+++ src/main/java/com/futureport/wiki/mapper/DocMapperCust.java
@@ -7,4 +7,6 @@ public interface DocMapperCust {
     void increaseViewCount(@Param("id") Long id);
 
     void increaseVoteCount(@Param("id") Long id);
+
+    void updateEbookInfo();
 }
--- src/main/java/com/futureport/wiki/service/DocService.java
+++ src/main/java/com/futureport/wiki/service/DocService.java
@@ -169,5 +169,9 @@ public class DocService {
         }
     }
 
+    public void updateEbookInfo(){
+        docMapperCust.updateEbookInfo();
+    }
+
 
 }
