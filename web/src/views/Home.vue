5db1fc34362dc53aaed7eb1751324dac946880b6
jrt-20 <76930558+jrt-20@users.noreply.github.com>
2021-08-02 00:23:44 +0800

    10-6 首页显示电子书信息：文档数、阅读数、点赞数

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -34,10 +34,20 @@
         <template #renderItem="{ item }">
           <a-list-item key="item.name">
             <template #actions>
-          <span v-for="{ type, text } in actions" :key="type">
-            <component v-bind:is="type" style="margin-right: 8px" />
-            {{ text }}
+
+          <span>
+                <component v-bind:is="'FileOutlined'" style="margin-right: 8px" />
+                {{ item.docCount }}
+              </span>
+              <span>
+                <component v-bind:is="'UserOutlined'" style="margin-right: 8px" />
+                {{ item.viewCount }}
+              </span>
+              <span>
+                <component v-bind:is="'LikeOutlined'" style="margin-right: 8px" />
+                {{ item.voteCount }}
           </span>
+
             </template>
             <a-list-item-meta :description="item.description">
               <template #title>
@@ -149,11 +159,11 @@ export default defineComponent({
         },
         pageSize: 12
       },
-      actions: [
-        { type: 'StarOutlined', text: '156' },
-        { type: 'LikeOutlined', text: '156' },
-        { type: 'MessageOutlined', text: '2' }
-      ],
+      // actions: [
+      //   { type: 'StarOutlined', text: '156' },
+      //   { type: 'LikeOutlined', text: '156' },
+      //   { type: 'MessageOutlined', text: '2' }
+      // ],
       handleClick,
       level1,
       isShowWelcome,
