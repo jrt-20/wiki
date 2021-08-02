7c14f06f9c56777f44951df0cc9d43f5adf11a37
jrt-20 <76930558+jrt-20@users.noreply.github.com>
2021-08-02 15:27:55 +0800

    11-6 后端增加统计数值接口

--- src/main/java/com/futureport/wiki/controller/EbookSnapshotController.java
+++ src/main/java/com/futureport/wiki/controller/EbookSnapshotController.java
@@ -1,12 +1,29 @@
 package com.futureport.wiki.controller;
 
+import com.futureport.wiki.resp.CommonResp;
+import com.futureport.wiki.resp.StatisticResp;
+import com.futureport.wiki.service.EbookSnapshotService;
+import com.sun.org.glassfish.external.statistics.Statistic;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
 
 
 @RestController
 @RequestMapping("/ebook-snapshot")
 public class EbookSnapshotController {
 
+    @Resource
+    private EbookSnapshotService ebookSnapshotService;
 
+    @GetMapping("/get-statistic")
+    public CommonResp getStatistic() {
+        List<StatisticResp> statisticResp = ebookSnapshotService.getStatistic();
+        CommonResp<List<StatisticResp>> commonResp = new CommonResp<>();
+        commonResp.setContent(statisticResp);
+        return commonResp;
+    }
 }
